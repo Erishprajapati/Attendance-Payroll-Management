d2df5fb3ac46e0ea067c5c062814e338ce1c0a33
Erish <irishmjn@gmail.com>
2025-11-26 13:36:52 +0545

    Updated the api urls for attendance views

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,10 +1,7 @@
+from .views import LoginAPI, UserRegistrationView, VerifyEmail
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView
-)
-from .serializers import CustomTokenObtainPair
 urlpatterns = [
-    # path('api/login/', CustomTokenObtainPair.as_view(), name="token_obtain_pair"), #login
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh_view') #refresh
+    path('login/', LoginAPI.as_view(), name='login'),
+    path('register/', UserRegistrationView.as_view(), name='signup'),
+    path("verify/<path:token>/",VerifyEmail.as_view())
 ]
